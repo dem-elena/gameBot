3087f7be98647d76889125c95473f5bcecb200fc
Elena Demyashina <dem-ea@yandex.ru>
2022-02-03 16:55:29 +0300

    вроде сделала доп задание 6 урока

--- js/bot.js
+++ js/bot.js
@@ -1,24 +1,38 @@
 "use strict";
-const gameBot = (findNumber) => {
+const gameBot = (findNumber, attemp) => {
+  let rezult;
   function find() {
-    let myNum = prompt("Угадай число от 1 до 100");
-    if (myNum == undefined) {
-      alert("Игра окончена");
-    } else {
-      if (isNaN(parseInt(myNum)) && !isFinite(myNum)) {
-        alert("Введи число!");
-        find();
+    if (attemp > 0) {
+      let myNum = prompt("Угадай число от 1 до 100");
+      if (myNum == undefined) {
+        alert("Игра окончена");
+        rezult = false;
+      } else {
+        if (isNaN(parseInt(myNum)) && !isFinite(myNum)) {
+          alert("Введи число!");
+          find();
+        }
+        myNum = parseInt(myNum);
+        if (myNum !== findNumber) {
+          attemp--;
+          myNum > findNumber
+            ? alert(`Загаданное число меньше, осталось попыток ${attemp}`)
+            : alert(`Загаданное число больше, осталось попыток ${attemp}`);
+          find();
+        } else {
+          rezult = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+        }
       }
-      myNum = parseInt(myNum);
-      if (myNum !== findNumber) {
-        myNum > findNumber
-          ? alert("Загаданное число меньше")
-          : alert("Загаданное число больше");
-        find();
-      } else alert("Угадал!");
+    } else {
+      rezult = confirm("Попытки закончились, хотите сыграть еще?");
     }
   }
-
   find();
+  return rezult;
 };
-gameBot(parseInt(Math.random() * 100));
+const gameBotdop = () => {
+  if (gameBot(parseInt(Math.random() * 100), 10) === true) {
+    gameBotdop();
+  }
+};
+gameBotdop();
